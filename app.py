602c5efc961a2f9ff6bcaf37ebdacf6f4eaaa1cd
thee yong <tyheng2003@gmail.com>
2025-08-26 17:51:48 +0800

    twelveth commit

--- app.py
+++ app.py
@@ -167,6 +167,6 @@ def health_check():
     return jsonify({'status': 'ok'})
 
 if __name__ == '__main__':
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host="0.0.0.0", port=port)
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    # app.run(port=5000)
